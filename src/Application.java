f87946da2eb023b349afac10c047abb156591815
Thomas Maury <tmaury@pcalb-mm0000.univ-jfc.fr>
2020-02-05 15:08:23 +0100

    ajout d'un titre à la fenêtre

--- src/Application.java
+++ src/Application.java
@@ -18,7 +18,7 @@ public class Application extends Frame implements WindowListener {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
+		this.setTitle("Fap ien");
 		this.addWindowListener(this);
 		this.add(vue);
 		this.pack();
