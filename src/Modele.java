6d174da795026905dabdcf789d00c97b239ba5fa
Thomas Maury <tmaury@pcalb-mm0000.univ-jfc.fr>
2020-02-05 14:20:25 +0100

    première version du projet

---
+++ src/Modele.java
@@ -0,0 +1,17 @@
+import java.util.Observable;
+
+public class Modele extends Observable {
+
+	int compte;
+	
+	public Modele() {
+		this.compte = 0;
+	}
+
+	public void incrémente() {
+		this.compte += 1;
+		this.setChanged();
+		this.notifyObservers(String.valueOf(this.compte));
+	}
+	
+}
